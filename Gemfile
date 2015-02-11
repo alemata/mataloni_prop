878edcb84b8d2d4ed179bcce78a22cc0c272a201
alemata <amataloni@gmail.com>
2015-02-11 20:04:43 -0300

    Add active_admin

--- Gemfile
+++ Gemfile
@@ -29,6 +29,7 @@ gem 'spring',        group: :development
 gem 'devise'
 gem 'bootstrap-sass', '~> 3.3.1'
 gem 'pry'
+gem 'activeadmin', github: 'activeadmin'
 
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable,
+         :authentication_keys => [:username]
 
   belongs_to :building
 end
